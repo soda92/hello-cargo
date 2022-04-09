5a1c2da07eb09f650b94dd5e0175887f0128e924
SodaCris <wang-jiaxun@outlook.com>
2022-04-09 08:56:29 +0800

    add launch config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let x = 1;
+    let y = x + 1;
+    println!("{}", y);
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    println!("{}", months[0]);
     println!("Hello, world!");
 }
